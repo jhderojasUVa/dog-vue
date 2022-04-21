5a5f8f3c5855d8bf0475451b5925aa2cf4c624ff
Jesus A Hernandez de Rojas <jesusangel.hernandezderojas@sky.uk>
2022-04-21 16:12:41 +0100

    test(dog-api.test.js): breed list test
    Nearly forgot them!

--- test/unit/api/dog-api.test.js
+++ test/unit/api/dog-api.test.js
@@ -21,15 +21,18 @@ describe("dog-api", () => {
       });
     });
 
+    // do the call
     const response = await API.default.getDog();
 
+    // is called in the proper way
     expect(spyOnaxiosGet).toHaveBeenCalledWith(
       "https://dog.ceo/api/breeds/image/random"
     );
+    // and respond the mock the message (of course it does!)
     expect(response).toBe("foo");
   });
   it("getDog with dog respond with normal url", async () => {
-      const searchDog = 'foo';
+    const searchDog = "foo";
     // spy on call and return a faux entry
     const spyOnaxiosGet = vi.spyOn(axios, "get").mockImplementation(() => {
       return new Promise((resolve) => {
@@ -42,9 +45,58 @@ describe("dog-api", () => {
       });
     });
 
+    // do the call
     await API.default.getDog(searchDog);
 
-    expect(spyOnaxiosGet).toHaveBeenCalledWith(`https://dog.ceo/api/breed/${searchDog}/images`);
+    // check if is called in the proper way
+    expect(spyOnaxiosGet).toHaveBeenCalledWith(
+      `https://dog.ceo/api/breed/${searchDog}/images`
+    );
     // the response takes a random so it not needed to check it
+  });
+  it("breedList works and when called empty returns a list", async () => {
+    const spyOnaxiosGet = vi.spyOn(axios, "get").mockImplementation(() => {
+      return new Promise((resolve) => {
+        resolve({
+          data: {
+            status: "success",
+            message: "foo",
+          },
+        });
+      });
+    });
+
+    // do the call
+    const response = await API.default.getBreedList();
+
+    // check if is called properly
+    expect(spyOnaxiosGet).toHaveBeenCalledWith(
+      "https://dog.ceo/api/breeds/list/all"
+    );
+    // and responds from the mock the message
+    expect(response).toBe("foo");
+  });
+  it("breedList with a breed do the call and respond what is needed", async () => {
+    const breed = "something";
+    const spyOnaxiosGet = vi.spyOn(axios, "get").mockImplementation(() => {
+      return new Promise((resolve) => {
+        resolve({
+          data: {
+            status: "success",
+            message: "foo",
+          },
+        });
+      });
+    });
+
+    // do the call
+    const response = await API.default.getBreedList(breed);
+
+    // check if is called properly
+    expect(spyOnaxiosGet).toHaveBeenCalledWith(
+      `https://dog.ceo/api/breed/${breed}/list`
+    );
+    // and responds from the mock the message
+    expect(response).toBe("foo");
   });
 });
